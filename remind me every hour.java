eba2cd94a542c921de43ab261731da61e03f7e0f
ibrahim-debugger1 <86594870+ibrahim-debugger1@users.noreply.github.com>
2022-01-25 19:18:16 +0200

    Add files via upload

---
+++ remind me every hour.java
@@ -0,0 +1,27 @@
+package helloo;
+
+import java.awt.Dimension;
+import java.awt.Font;
+import java.time.LocalDateTime;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextPane;
+
+
+public class Helloo {
+
+   
+    public static void main(String[] args) throws InterruptedException {
+        JLabel jtp = new JLabel("Hey, move your body");
+        jtp.setFont(new Font("Serif", Font.PLAIN, 100));
+        jtp.setSize(new Dimension(1000, 1000));
+        jtp.setPreferredSize(new Dimension(1000, 1000));
+        while(true){
+            int currmin=LocalDateTime.now().getMinute();
+            int hey=((60-currmin)*60)*1000;
+            Thread.sleep(hey);
+            JOptionPane.showMessageDialog(null,jtp,"get up and run for a three min",JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+}
